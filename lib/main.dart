85173ab71e321cd29144e615b1abf9ca5497469f
Cagri Akgül <cagriakgul@windowslive.com>
2019-03-09 11:20:04 +0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:dua_app/screen/homescreen.dart';
+import 'package:flutter/material.dart';
+void main(){
+  runApp(
+    MaterialApp(
+      home: HomeDua(),
+    )
+  );
+}
---
+++ lib/screen/homescreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class HomeDua extends StatefulWidget {
+  @override
+  _HomeDuaState createState() => _HomeDuaState();
+}
+
+class _HomeDuaState extends State<HomeDua> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
