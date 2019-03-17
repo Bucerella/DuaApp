186f28d4e794a6996a7b80ca0f03c1d87c398425
Buse ERKUŞ <buseerkus@Buses-MacBook-Air.local>
2019-03-17 12:38:42 +0300

    Namaz nasıl kılınır kontrol edildi.

--- lib/screen/namaz_kilinisi/sabahnamazi.dart
+++ lib/screen/namaz_kilinisi/sabahnamazi.dart
@@ -17,35 +17,36 @@ class _SabahNamaziState extends State<SabahNamazi> {
           children: <Widget>[
             SizedBox(height: 10,),
             Text("Sabah Namazı Sünnet 1. Rekat",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,fontFamily: "Montserrat"),),
-            namazKilinisiCard(context, "namaz-3.jpg", "Niyet ettim Allah rızası için Sabah namazının dört rekat sünnetini kılmaya diye niyet ederiz. \"Allahu Ekber\" diyerek İftitah Tekbiri alır ve namaza başlarız."),
-            namazKilinisiCard(context, "namaz-2.jpg", "Sübhaneke'yi okuruz. Euzü-besmele çekeriz. Fatiha Suresini okuruz. Kur'an'dan bir sure okuruz."),
+            namazKilinisiCard(context, "namaz-3.jpg", "Niyet ettim Allah rızası için Sabah namazının iki rekat sünnetini kılmaya diye niyet ederiz. \"Allahu Ekber\" diyerek İftitah Tekbiri alır ve namaza başlarız."),
+            namazKilinisiCard(context, "namaz-2.jpg", "Eller bağlandıktan sonra Sübhaneke'yi okuruz. Euzü-besmele çekeriz. Fatiha Suresini okuruz. ve \"Amin\" deriz. Ardından Kur'an'dan bir sure okuruz. "),
             namazKilinisiCard(context, "namaz-4.jpg", "Rüku'ya gideriz. Rükûda üç defa \"Sübhâne Râbbiye'l-Azim\" dedikten sonra doğruluruz. Doğrulurken \"Semi Allahu li-men hamideh\" deriz."),
             namazKilinisiCard(context, "namaz-1.jpg", "Tam doğrulunca da \"Rabbenâ leke'l hamd\" deriz. \"Allahu Ekber\" deyip secdeye gideriz."),
+            namazKilinisiCard(context, "namaz-5.jpg", "Secdede üç defa : \"Sübhâne rabbiyel-a 'lâ\" deriz. \“Allahu Ekber\" diyerek doğrulup tekrar secdeye gidip, yine üç defa \"Sübhâne rabbiyel-a 'lâ\" deriz. Secdeden \"Allahu Ekber\" deyip ayağa kalkarız, böylece ikinci rekat başlamış olur."),
 
-            namazKilinisiCard(context, "namaz-5.jpg", "\"Allahu Ekber\" deyip secdeye gideriz. Secdede üç defa : \"Sübhâne rabbiyel-a 'lâ\" deriz. \“Allahu Ekber\" deyip: Tahiyyata otururuz."),
-            namazKilinisiCard(context, "namaz-7.jpg", "Oturduktan sonra Ettehiyyâtü duasını okuruz. Tahiyyattan \“Allahu Ekber”\ deyip ayağa kalkarız, böylece ikinci rekat başlamış olur. "),
             SizedBox(height: 10,),
-            Text("Sabah Namazı Sünnet 4. Rekat",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,fontFamily: "Montserrat"),),
-            namazKilinisiCard(context, "namaz-2.jpg", "Ayağa kalkınca elleri bağlayıp Besmele çekeriz: \”Bismillahirrahmanirrahim”\ Fatiha Suresi okur ve \”Amin”\ deriz, ardından da namaz surelerinden birini okuruz. Sonra \“Allahu Ekber”\ diyerek rükûya gideriz."),
-            namazKilinisiCard(context, "namaz-4.jpg", "Rükûda üç defa \"Sübhâne Râbbiye'l-Azim\" dedikten sonra doğruluruz. Doğrulurken \"Semi Allahu li-men hamideh\" deriz. Tam doğrulunca da \"Rabbenâ leke'l hamd\" deriz."),
-            namazKilinisiCard(context, "namaz-5.jpg", "\"Allahu Ekber\" deyip secdeye gideriz. Secdede üç defa : \"Sübhâne rabbiyel-a 'lâ\" deriz. \“Allahu Ekber\" deyip: Tahiyyata otururuz. Oturduktan sonra Ettehiyyâtü , Allâhumme salli , Allâhumme Bârik ve Rabbenâ dualarını okuruz."),
-            namazKilinisiCard(context, "namaz-6.jpg", "\"Es selâmu aleyküm ve rahmet'ullah\" diye önce sağa ve sonra sola selam vereriz böylece öğle namazının dört rekatlık sünneti sona ermiş olur."),
+            Text("Sabah Namazı Sünnet 2. Rekat",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,fontFamily: "Montserrat"),),
+            namazKilinisiCard(context, "namaz-2.jpg", "Ayağa kalkınca elleri bağlayıp Besmele çekeriz: \”Bismillahirrahmanirrahim”\ sonra Fatiha Suresi okur ve \”Amin”\ deriz, ardından da namaz surelerinden birini okuruz. Sonra \“Allahu Ekber”\ diyerek rükûya gideriz."),
+            namazKilinisiCard(context, "namaz-4.jpg", "Rükûda üç defa \"Sübhâne Râbbiye'l-Azim\" dedikten sonra doğruluruz. Doğrulurken \"Semi Allahu li-men hamideh\" deriz."),
+            namazKilinisiCard(context, "namaz-1.jpg", "Tam doğrulunca da \"Rabbenâ leke'l hamd\" deriz. \"Allahu Ekber\" deyip secdeye gideriz."),
+
+            namazKilinisiCard(context, "namaz-5.jpg", "Secdede üç defa : \"Sübhâne rabbiyel-a 'lâ\" deriz. \“Allahu Ekber\" diyerek doğruluruz yeniden,üç defa : \"Sübhâne rabbiyel-a 'lâ\" deyip: Tahiyyata otururuz. Oturduktan sonra Ettehiyyâtü, Allâhumme salli, Allâhumme Bârik ve Rabbenâ dualarını okuruz."),
+            namazKilinisiCard(context, "namaz-6.jpg", "\"Es selâmu aleyküm ve rahmet'ullah\" diye önce sağa ve sonra sola selam vereriz böylece sabah namazının iki rekatlık sünneti sona ermiş olur."),
             SizedBox(height: 10,),
             SizedBox(height: 10,),
             Text("Sabah Namazı Farz 1. Rekat",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,fontFamily: "Montserrat"),),
-            namazKilinisiCard(context, "namaz-3.jpg", "Niyet ettim Allah rızası için Sabah namazının dört rekat farzını kılmaya diye niyet ederiz. \"Allahu Ekber\" diyerek İftitah Tekbiri alır ve namaza başlarız."),
-            namazKilinisiCard(context, "namaz-2.jpg", "Sübhaneke'yi okuruz. Euzü-besmele çekeriz. Fatiha Suresini okuruz. Kur'an'dan bir sure okuruz."),
-            namazKilinisiCard(context, "namaz-4.jpg", "Rüku'ya gideriz. Rükûda üç defa \"Sübhâne Râbbiye'l-Azim\" dedikten sonra doğruluruz. Doğrulurken \"Semi Allahu li-men hamideh\" deriz."),
+            namazKilinisiCard(context, "namaz-3.jpg", "Niyet ettim Allah rızası için Sabah namazının iki rekat farzını kılmaya diye niyet ederiz. \"Allahu Ekber\" diyerek İftitah Tekbiri alır ve namaza başlarız."),
+            namazKilinisiCard(context, "namaz-2.jpg", "Elleri bağladıktan sonra Sübhaneke'yi okuruz. Euzü-besmele çekeriz. Fatiha Suresini okuruz. Sonra Kur'an'dan bir sure okuruz. Rüku'ya gideriz."),
+            namazKilinisiCard(context, "namaz-4.jpg", "Rükûda üç defa \"Sübhâne Râbbiye'l-Azim\" dedikten sonra doğruluruz. Doğrulurken \"Semi Allahu li-men hamideh\" deriz."),
             namazKilinisiCard(context, "namaz-1.jpg", "Tam doğrulunca da \"Rabbenâ leke'l hamd\" deriz. \"Allahu Ekber\" deyip secdeye gideriz."),
+            namazKilinisiCard(context, "namaz-5.jpg", "Secdede üç defa : \"Sübhâne rabbiyel-a 'lâ\" deriz. \“Allahu Ekber\" deyip, doğrulup tekrar secdeye gidip, \"Sübhâne rabbiyel-a 'lâ\" dedikten sonra \“Allahu Ekber”\ deyip ayağa kalkarız, böylece ikinci rekat başlamış olur."),
 
-            namazKilinisiCard(context, "namaz-5.jpg", "\"Allahu Ekber\" deyip secdeye gideriz. Secdede üç defa : \"Sübhâne rabbiyel-a 'lâ\" deriz. \“Allahu Ekber\" deyip: Tahiyyata otururuz."),
-            namazKilinisiCard(context, "namaz-7.jpg", "Oturduktan sonra Ettehiyyâtü duasını okuruz. Tahiyyattan \“Allahu Ekber”\ deyip ayağa kalkarız, böylece ikinci rekat başlamış olur. "),
             SizedBox(height: 10,),
-            Text("Sabah Namazı Farz 4. Rekat",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,fontFamily: "Montserrat"),),
-            namazKilinisiCard(context, "namaz-2.jpg", "Ayağa kalkınca elleri bağlayıp Besmele çekeriz: \”Bismillahirrahmanirrahim”\ Fatiha Suresi okur ve \”Amin”\ deriz, ardından da namaz surelerinden birini okuruz. Sonra \“Allahu Ekber”\ diyerek rükûya gideriz."),
-            namazKilinisiCard(context, "namaz-4.jpg", "Rükûda üç defa \"Sübhâne Râbbiye'l-Azim\" dedikten sonra doğruluruz. Doğrulurken \"Semi Allahu li-men hamideh\" deriz. Tam doğrulunca da \"Rabbenâ leke'l hamd\" deriz."),
-            namazKilinisiCard(context, "namaz-5.jpg", "\"Allahu Ekber\" deyip secdeye gideriz. Secdede üç defa : \"Sübhâne rabbiyel-a 'lâ\" deriz. \“Allahu Ekber\" deyip: Tahiyyata otururuz. Oturduktan sonra Ettehiyyâtü , Allâhumme salli , Allâhumme Bârik ve Rabbenâ dualarını okuruz."),
-            namazKilinisiCard(context, "namaz-6.jpg", "\"Es selâmu aleyküm ve rahmet'ullah\" diye önce sağa ve sonra sola selam vereriz böylece öğle namazının dört rekatlık sünneti sona ermiş olur."),
+            Text("Sabah Namazı Farz 2. Rekat",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,fontFamily: "Montserrat"),),
+            namazKilinisiCard(context, "namaz-2.jpg", "Ayağa kalkınca elleri bağlayıp Besmele çekeriz: \”Bismillahirrahmanirrahim”\ Fatiha Suresi okur ve \”Amin”\ deriz, sonra Kur'an'dan bir sure okuruz. Sonra \“Allahu Ekber”\ diyerek rükûya gideriz."),
+            namazKilinisiCard(context, "namaz-4.jpg", "Rükûda üç defa \"Sübhâne Râbbiye'l-Azim\" dedikten sonra doğruluruz. Doğrulurken \"Semi Allahu li-men hamideh\" deriz."),
+            namazKilinisiCard(context, "namaz-1.jpg", "Tam doğrulunca da \"Rabbenâ leke'l hamd\" deriz. \"Allahu Ekber\" deyip secdeye gideriz."),
+            namazKilinisiCard(context, "namaz-5.jpg", "Secdede üç defa : \"Sübhâne rabbiyel-a 'lâ\" deriz. \“Allahu Ekber\" deyip, doğrulup tekrar secdeye gidip, üç defa daha \"Sübhâne rabbiyel-a 'lâ\" dedikten sonra Tahiyyata otururuz. Oturduktan sonra Ettehiyyâtü, Allâhumme salli, Allâhumme Bârik ve Rabbenâ dualarını okuruz."),
+            namazKilinisiCard(context, "namaz-6.jpg", "\"Es selâmu aleyküm ve rahmet'ullah\" diye önce sağa ve sonra sola selam vereriz böylece sabah namazının iki rekatlık farzı sona ermiş olur."),
             SizedBox(height: 11,),
 
 
@@ -55,4 +56,6 @@ class _SabahNamaziState extends State<SabahNamazi> {
       ),
     );
   }
+
+  _SabahNamaziState();
 }
